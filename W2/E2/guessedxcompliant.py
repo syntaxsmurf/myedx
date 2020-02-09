f3f4e6e900d5abfff887204adfc8016c1489519d
[syntaxsmurf] <[osdonline@gmail.com]>
2020-02-09 22:04:10 +0100

    refactor

--- W2/E2/guessedxcompliant.py
+++ W2/E2/guessedxcompliant.py
@@ -1,29 +1,29 @@
 low = 0
 high = 100
-userN = abs(int((low + high) / 2))
+userN = int((low + high) / 2)
 userC = ""
 userIn = ""
 
-print(f"Please think of a number between {low} and {high}")
+print("Please think of a number between" , low , "and" , high)
 print("please enter h if your number is higher, l if your number is lower, c if the number is correct")
 
 while userIn != "c" or "C":
-    userIn = input(f"Is your secret number {userN}?:")
+    userIn = input("Is your secret number " + str(userN) + " ?:")
     if userIn == "c" or userIn == "C":
         userC = userN
         break
     elif userIn == "l" or userIn == "L":
        #what happens if lower
         high = userN
-        userN = abs(int((low + high) / 2))
+        userN = int((low + high) / 2)
         #print(f"this is l test output {userN} low: {low} high: {high}")
     elif userIn == "h" or userIn == "H":
         #what happens if higher
         low = userN
-        userN = abs(int((low + high) / 2))
+        userN = int((low + high) / 2)
         #print(f"this is h test output {userN} low: {low} high: {high}")
 
     else:
         print("I am sorry please enter h if your number is higher, l if your number is lower, c if the number is correct")
 
-print(f"Secret number is {userC}")
+print(f"Secret number is" , userC)
