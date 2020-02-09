520d824a5494b207f8f2fe69c09a65be68f58e09
[syntaxsmurf] <[osdonline@gmail.com]>
2020-02-09 21:09:59 +0100

    it works edx dont like it though?

--- W2/E2/guessn.py
+++ W2/E2/guessn.py
@@ -16,12 +16,12 @@ while userIn != "c" or "C":
        #what happens if lower
         high = userN
         userN = abs(int((low + high) / 2))
-        print(f"this is l test output {userN} low: {low} high: {high}")
+        #print(f"this is l test output {userN} low: {low} high: {high}")
     elif userIn == "h" or userIn == "H":
         #what happens if higher
         low = userN
         userN = abs(int((low + high) / 2))
-        print(f"this is h test output {userN} low: {low} high: {high}")
+        #print(f"this is h test output {userN} low: {low} high: {high}")
 
     else:
         print("I am sorry please enter h if your number is higher, l if your number is lower, c if the number is correct")
