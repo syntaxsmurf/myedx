1ab31678f42a7b7d977156402f82d4f237b907d8
[syntaxsmurf] <[osdonline@gmail.com]>
2020-02-09 22:55:09 +0100

    inverted logic fixed it

--- W2/E2/guessedxcompliant.py
+++ W2/E2/guessedxcompliant.py
@@ -5,25 +5,24 @@ userC = ""
 userIn = ""
 
 print("Please think of a number between" , low , "and" , high)
-print("please enter h if your number is higher, l if your number is lower, c if the number is correct")
-
 while userIn != "c" or "C":
-    userIn = input("Is your secret number " + str(userN) + " ?:")
+    userIn = input("Is your secret number " + str(userN) + "?\nEnter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")
     if userIn == "c" or userIn == "C":
         userC = userN
         break
-    elif userIn == "l" or userIn == "L":
-       #what happens if lower
+    elif userIn == "h" or userIn == "H": 
+        #what happens if higher
         high = userN
         userN = int((low + high) / 2)
-        #print(f"this is l test output {userN} low: {low} high: {high}")
-    elif userIn == "h" or userIn == "H":
-        #what happens if higher
+        #print(f"this is h test output {userN} low: {low} high: {high}")
+    elif userIn == "l" or userIn == "L":
+
+        #what happens if lower
         low = userN
         userN = int((low + high) / 2)
-        #print(f"this is h test output {userN} low: {low} high: {high}")
+        #print(f"this is l test output {userN} low: {low} high: {high}")
 
     else:
         print("I am sorry please enter h if your number is higher, l if your number is lower, c if the number is correct")
 
-print("Secret number is" + str(userC))
+print("Game over. Your secret number was: " + str(userC))
